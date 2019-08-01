963e1299761302849f54531e0a9a800033fd4bf3
silvereyes <silvereyes333@gmail.com>
2019-08-01 18:00:08 -0500

    Version 1.4.4
    - API bump for Update 23 Scalebreaker

--- DetailedResearchScrolls.lua
+++ DetailedResearchScrolls.lua
@@ -1,7 +1,7 @@
 DetailedResearchScrolls = {
     name = "DetailedResearchScrolls",
     title = "Detailed Research Scrolls",
-    version = "1.4.3",
+    version = "1.4.4",
     author = "silvereyes",
 }
 local addon                 = DetailedResearchScrolls
