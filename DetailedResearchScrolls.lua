743c465a0f330114c67fda62abf4c1d7a6361f8c
silvereyes <silvereyes333@gmail.com>
2020-02-16 17:01:02 -0600

    Version 1.4.6
    - Update 25 Harrowstorm compatibility
    - This version is compatible with both PTS (Harrowstorm) and live (Dragonhold)

--- DetailedResearchScrolls.lua
+++ DetailedResearchScrolls.lua
@@ -1,7 +1,7 @@
 DetailedResearchScrolls = {
     name = "DetailedResearchScrolls",
     title = "Detailed Research Scrolls",
-    version = "1.4.5",
+    version = "1.4.6",
     author = "silvereyes",
 }
 local addon                 = DetailedResearchScrolls
